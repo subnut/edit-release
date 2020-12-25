d741baaa838cf9f29b9e04f05343e914495f7e4d
Subhaditya Nath <sn03.general@gmail.com>
2020-12-26 01:13:16 +0530

    v4

--- src/edit-release.js
+++ src/edit-release.js
@@ -12,19 +12,22 @@ async function run() {
     const { owner: currentOwner, repo: currentRepo } = context.repo;
 
     // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-    const tagName = core.getInput('tag_name', { required: true });
+    const tagName = core.getInput('tag_name', { required: false });
 
     // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.10.15' to 'v1.10.15'
     const tag = tagName.replace('refs/tags/', '');
+
+    const owner = core.getInput('owner', { required: false }) || currentOwner;
+    const repo = core.getInput('repo', { required: false }) || currentRepo;
+
+    const bodyPath = core.getInput('body_path', { required: false });
+
     const releaseName = core.getInput('release_name', { required: false }).replace('refs/tags/', '');
     const body = core.getInput('body', { required: false });
     const draft = core.getInput('draft', { required: false }) === 'true';
     const prerelease = core.getInput('prerelease', { required: false }) === 'true';
     const commitish = core.getInput('commitish', { required: false }) || context.sha;
 
-    const bodyPath = core.getInput('body_path', { required: false });
-    const owner = core.getInput('owner', { required: false }) || currentOwner;
-    const repo = core.getInput('repo', { required: false }) || currentRepo;
     let bodyFileContent = null;
     if (bodyPath !== '' && !!bodyPath) {
       try {
@@ -36,35 +39,46 @@ async function run() {
 
 
    // Get the release id
-   const { id: releaseID } = await octokit.repos.getReleaseByTag({
+   const getReleaseResponse = await octokit.repos.getReleaseByTag({
       owner,
       repo,
       tag
     });
+    const {
+      data: {
+        id: releaseID,
+        name: releaseNameOld,
+        body: bodyOld,
+        draft: draftOld,
+        prerelease: prereleaseOld,
+        target_commitish: commitishOld
+      }
+    } = getReleaseResponse;
+    console.log("Release id:", releaseID);
 
     // Edit the release
     // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
     // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
+    console.log(owner);
+    console.log(repo);
+    console.log(releaseID);
     const updateReleaseResponse = await octokit.repos.updateRelease({
       owner,
       repo,
-      release_id: releaseID,
-      name: releaseName,
-      body: bodyFileContent || body,
-      draft,
-      prerelease,
-      target_commitish: commitish
+      releaseID,
+      prerelease: false
     });
+    console.log(updateReleaseResponse);
 
     // Get the ID, html_url, and upload URL for the updated Release from the response
     const {
-      data: { id: releaseId, html_url: htmlUrl, upload_url: uploadUrl }
+      data: { id: releaseIdUpdated, html_url: htmlUrlUpdated, upload_url: uploadUrlUpdated }
     } = updateReleaseResponse;
 
     // Set the output variables for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-    core.setOutput('id', releaseId);
-    core.setOutput('html_url', htmlUrl);
-    core.setOutput('upload_url', uploadUrl);
+    core.setOutput('id', releaseIdUpdated);
+    core.setOutput('html_url', htmlUrlUpdated);
+    core.setOutput('upload_url', uploadUrlUpdated);
   } catch (error) {
     core.setFailed(error.message);
   }
